da16ccdcde6e13025387afbecde085e6cf398def
lucas <lucas.2410460601074@aluno.faeterj-prc.faetec.rj.gov.br>
2024-06-07 16:33:29 -0300

    exercicios 4 e 5 da lista3 concluidos

--- lista3/exerc4.c
+++ lista3/exerc4.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main(){
+    float soma = 0;
+    int n=0;
+
+    printf("Insira o valor de n:");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        soma+=(float)1/i;
+    }
+
+    printf("O valor da serie até %d é %.2f",n,soma);
+
     return 0;
 }
---
+++ lista3/exerc5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+    int pares =0;
+    int min,max;
+    int i=0,c=0;
+
+    while (1){
+        do{
+            printf("Insira um número:");
+            printf("Entrei %d\n",i);
+            scanf("%d",&c);
+            if(c==-1){
+                break;
+            }    
+        }while (!(c>=0 && c<=10));
+
+        if(c==-1){
+            break;
+        } 
+
+        c % 2==0 && pares++;
+        if(i == 0) {
+            printf("Entrei\n");
+            min = c;
+            max = c;
+        }else if(min < c){
+            max = c;            
+        }else{
+            max = min;
+            min = c;
+        }
+
+        i++;
+    }    
+
+    printf("Números digitados: %d\n",i);
+    printf("Media de pares: %.2f\n",(float)pares/i);
+    printf("Menor numero: %d\n",min);
+    printf("Maior numero: %d\n",max);
+
+    return 0;
+}
